1f0c33c85d30e0b1aa6dee9e66419bd132796761
S. A <salvatore.anania@laplateforme.io>
2023-03-16 20:49:28 +0100

    Ajout du chronometre et amelioration graphique

--- main.py
+++ main.py
@@ -12,16 +12,16 @@ def afficher_heure(heure):
     secondes=heure[2]
     current_time=(heures,minutes,secondes)
     if mode=="":
-        affiche_heure.config(text=str(current_time[0])+":"+str(current_time[1])+":"+str(current_time[2]))
+        affiche_heure.config(text=str("{:02d}".format(current_time[0]))+":"+str("{:02d}".format(current_time[1]))+":"+str("{:02d}".format(current_time[2])))
     else:
         if heures==0:
-            affiche_heure.config(text=str(12)+":"+str(current_time[1])+":"+str(current_time[2])+"AM")
+            affiche_heure.config(text=str(12)+":"+str("{:02d}".format(current_time[1]))+":"+str("{:02d}".format(current_time[2]))+"AM")
         elif heures<12:
-            affiche_heure.config(text=str(current_time[0])+":"+str(current_time[1])+":"+str(current_time[2])+"AM")
+            affiche_heure.config(text=str(current_time[0])+":"+str("{:02d}".format(current_time[1]))+":"+str("{:02d}".format(current_time[2]))+"AM")
         elif heures==12:
-             affiche_heure.config(text=str(12)+":"+str(current_time[1])+":"+str(current_time[2])+"PM")
+             affiche_heure.config(text=str(12)+":"+str("{:02d}".format(current_time[1]))+":"+str("{:02d}".format(current_time[2]))+"PM")
         else:
-            affiche_heure.config(text=str(current_time[0]%12)+":"+str(current_time[1])+":"+str(current_time[2])+"PM")
+            affiche_heure.config(text=str(current_time[0]%12)+":"+str("{:02d}".format(current_time[1]))+":"+str("{:02d}".format(current_time[2]))+"PM")
     fenetre.update()
     if secondes<59:
         secondes+=1
@@ -35,6 +35,7 @@ def afficher_heure(heure):
                 heures=0
     current_time=(heures,minutes,secondes)
     my_alarm(alarm)
+    chrono()
     if instance==1:
         fenetre.after(1000,lambda: afficher_heure(current_time))
     elif instance!=3:
@@ -78,8 +79,35 @@ def my_alarm(heure_alarm):
             affiche_alarme.pack(side = TOP,padx=5, pady=5)
         else:
             affiche_alarme.pack_forget()
+            
+def chrono():
+    global chrono_run
+    global chrono_time
+    if chrono_run==True:
+        if chrono_time[2]<59:
+            chrono_time[2]+=1
+        else:
+            chrono_time[2]=0
+            chrono_time[1]+=1
+            if chrono_time[1]>59:
+                chrono_time[1]=0
+                chrono_time[0]+=1
+                if chrono_time[0]>23:
+                    chrono_time[0]=0
+        affiche_chrono.config(text=chrono_time)
 
-        
+def set_chrono():
+    global chrono_run
+    global chrono_time
+    if chrono_run==False:
+        chrono_run=True
+        affiche_chrono.pack(side = TOP,padx=5, pady=5)
+    else:
+        chrono_run=False
+        for i in range(len(chrono_time)):
+            chrono_time[i]=0
+        affiche_chrono.pack_forget()
+        affiche_chrono.config(text=chrono_time)
         
     
 
@@ -92,59 +120,64 @@ fenetre.geometry("800x600")
 Frame_horloge= Frame(fenetre,bg='grey',width=400)
 
 global mode
+chrono_time=[0,59,54]
 mode= ""
-
-affiche_heure=Label(Frame_horloge, text="current_time")
+chrono_run=False
+affiche_heure=Label(Frame_horloge, text="current_time", bg="grey",font=("Arial", 100))
 affiche_heure.pack(side = TOP,padx=5, pady=5)
 
 
 frame_input_time= Frame(Frame_horloge,bg='grey',width=400)
-set_heure=Entry(frame_input_time, text="",width=4)
+set_heure=Entry(frame_input_time, text="",width=4,font=("Arial", 15))
 set_heure.pack(side=LEFT,padx=5)
-set_heure_label=Label(frame_input_time, text="H")
+set_heure_label=Label(frame_input_time, text="H",font=("Arial", 20))
 set_heure_label.pack(side=LEFT,padx=5)
-set_minute=Entry(frame_input_time, text="",width=4)
+set_minute=Entry(frame_input_time, text="",width=4,font=("Arial", 15))
 set_minute.pack(side=LEFT,padx=5)
-set_minute_label=Label(frame_input_time, text="M")
+set_minute_label=Label(frame_input_time, text="M",font=("Arial", 20))
 set_minute_label.pack(side=LEFT,padx=5)
-set_seconde=Entry(frame_input_time, text="",width=4)
+set_seconde=Entry(frame_input_time, text="",width=4,font=("Arial", 15))
 set_seconde.pack(side=LEFT,padx=5)
-set_seconde_label=Label(frame_input_time, text="S")
+set_seconde_label=Label(frame_input_time, text="S",font=("Arial", 20))
 set_seconde_label.pack(side=LEFT,padx=5)
 
 frame_input_time.pack(pady=5)
 
-lancer_heure=Button(Frame_horloge, text="Regler heure", command= instantiation)
+lancer_heure=Button(Frame_horloge, text="Regler heure",font=("Arial", 15), command= instantiation)
 lancer_heure.pack()
 
 frame_input_alarm= Frame(Frame_horloge,bg='grey',width=400)
-set_H_alarm=Entry(frame_input_alarm, text="",width=4)
+set_H_alarm=Entry(frame_input_alarm, text="",width=4,font=("Arial", 15))
 set_H_alarm.pack(side=LEFT,padx=5)
-set_heure_label_alarm=Label(frame_input_alarm, text="H")
+set_heure_label_alarm=Label(frame_input_alarm, text="H",font=("Arial", 20))
 set_heure_label_alarm.pack(side=LEFT,padx=5)
-set_M_alarm=Entry(frame_input_alarm, text="",width=4)
+set_M_alarm=Entry(frame_input_alarm, text="",width=4,font=("Arial", 15))
 set_M_alarm.pack(side=LEFT,padx=5)
-set_minute_label_alarm=Label(frame_input_alarm, text="M")
+set_minute_label_alarm=Label(frame_input_alarm, text="M",font=("Arial", 20))
 set_minute_label_alarm.pack(side=LEFT,padx=5)
-set_S_alarm=Entry(frame_input_alarm, text="",width=4)
+set_S_alarm=Entry(frame_input_alarm, text="",width=4,font=("Arial", 15))
 set_S_alarm.pack(side=LEFT,padx=5)
-set_seconde_label_alarm=Label(frame_input_alarm, text="S")
+set_seconde_label_alarm=Label(frame_input_alarm, text="S",font=("Arial", 20))
 set_seconde_label_alarm.pack(side=LEFT,padx=5)
 frame_input_alarm.pack(pady=5)
 
-lancer_alarm=Button(Frame_horloge, text="Regler alarme", command=lambda: my_alarm((int(set_H_alarm.get()),int(set_M_alarm.get()),int(set_S_alarm.get()))))
+lancer_alarm=Button(Frame_horloge, text="Regler alarme",font=("Arial", 15), command=lambda: my_alarm((int(set_H_alarm.get()),int(set_M_alarm.get()),int(set_S_alarm.get()))))
 lancer_alarm.pack(side = TOP,padx=5, pady=5)
 affiche_alarme=Label(Frame_horloge, text="Alarme activé",bg="red")
-lancer_alarm=Button(Frame_horloge, text="Pause/reprise", command= pause)
+lancer_alarm=Button(Frame_horloge, text="Pause/reprise",font=("Arial", 15), command= pause)
 lancer_alarm.pack(side = TOP,padx=5, pady=5)
 Frame_horloge.pack()
 
-mode_button=Button(Frame_horloge, text="Mode d'affichage", command= time_mode)
+mode_button=Button(Frame_horloge, text="Mode d'affichage",font=("Arial", 15), command= time_mode)
 mode_button.pack(side = TOP,padx=5, pady=5)
+
+mode_button=Button(Frame_horloge, text="Chronometre",font=("Arial", 15), command= set_chrono)
+mode_button.pack(side = TOP,padx=5, pady=5)
+affiche_chrono=Label(Frame_horloge, text=chrono_time, bg="grey",font=("Arial", 80))
 
 my_alarm((30,0,0))
 current = time.strftime("%H,%M,%S")
 res = tuple(map(int, current.split(',')))
-test=list(res)
-afficher_heure(test)
+afficher_heure(res)
+
 fenetre.mainloop()
