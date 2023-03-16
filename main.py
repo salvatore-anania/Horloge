d4e203024a5d23e8a19c1e555b119b3ac4b3b833
S. A <salvatore.anania@laplateforme.io>
2023-03-16 19:54:04 +0100

    changement graphique pour meilleur experience utilisateur

--- main.py
+++ main.py
@@ -10,16 +10,6 @@ def afficher_heure(heure):
     heures=heure[0]
     minutes=heure[1]
     secondes=heure[2]
-    if secondes<59:
-        secondes+=1
-    else:
-        secondes=0
-        minutes+=1
-        if minutes>59:
-            minutes=0
-            heures+=1
-            if heures>23:
-                heures=0
     current_time=(heures,minutes,secondes)
     if mode=="":
         affiche_heure.config(text=str(current_time[0])+":"+str(current_time[1])+":"+str(current_time[2]))
@@ -33,6 +23,17 @@ def afficher_heure(heure):
         else:
             affiche_heure.config(text=str(current_time[0]%12)+":"+str(current_time[1])+":"+str(current_time[2])+"PM")
     fenetre.update()
+    if secondes<59:
+        secondes+=1
+    else:
+        secondes=0
+        minutes+=1
+        if minutes>59:
+            minutes=0
+            heures+=1
+            if heures>23:
+                heures=0
+    current_time=(heures,minutes,secondes)
     my_alarm(alarm)
     if instance==1:
         fenetre.after(1000,lambda: afficher_heure(current_time))
@@ -42,13 +43,8 @@ def afficher_heure(heure):
 def instantiation():
     global instance
     instance+=1
-    temp=list(map(int, regler_heure.get().split(',')))
-    if temp[2]!=0:
-        temp[2]-=1
-        envoi=tuple(temp)
-    else:
-        envoi=temp
-    afficher_heure(envoi)
+    temp=(int(set_heure.get()),int(set_minute.get()),int(set_seconde.get()))
+    afficher_heure(temp)
     
 def pause():
     global instance
@@ -91,27 +87,54 @@ def my_alarm(heure_alarm):
 fenetre = Tk()
 fenetre.title("Horloge")
 fenetre.configure(bg='grey')
+fenetre.geometry("800x600")
 
-Frame_horloge= Frame(fenetre,bg='grey')
+Frame_horloge= Frame(fenetre,bg='grey',width=400)
 
 global mode
 mode= ""
 
-Frame_user = Frame(Frame_horloge, borderwidth=2, relief=GROOVE)
-Frame_user.pack( padx=30, pady=30)
 affiche_heure=Label(Frame_horloge, text="current_time")
 affiche_heure.pack(side = TOP,padx=5, pady=5)
 
-regler_heure=Entry(Frame_horloge, textvariable="Entrez heure")
-regler_heure.pack(side = TOP,padx=5, pady=5)
-lancer_heure=Button(Frame_horloge, text="Regler heure", command= instantiation)
-lancer_heure.pack(side = TOP,padx=5, pady=5)
 
-regler_alarm=Entry(Frame_horloge, text="current_time")
-regler_alarm.pack(side = TOP,padx=5, pady=5)
-lancer_alarm=Button(Frame_horloge, text="Regler alarme", command=lambda: my_alarm(tuple(map(int, regler_alarm.get().split(',')))))
+frame_input_time= Frame(Frame_horloge,bg='grey',width=400)
+set_heure=Entry(frame_input_time, text="",width=4)
+set_heure.pack(side=LEFT,padx=5)
+set_heure_label=Label(frame_input_time, text="H")
+set_heure_label.pack(side=LEFT,padx=5)
+set_minute=Entry(frame_input_time, text="",width=4)
+set_minute.pack(side=LEFT,padx=5)
+set_minute_label=Label(frame_input_time, text="M")
+set_minute_label.pack(side=LEFT,padx=5)
+set_seconde=Entry(frame_input_time, text="",width=4)
+set_seconde.pack(side=LEFT,padx=5)
+set_seconde_label=Label(frame_input_time, text="S")
+set_seconde_label.pack(side=LEFT,padx=5)
+
+frame_input_time.pack(pady=5)
+
+lancer_heure=Button(Frame_horloge, text="Regler heure", command= instantiation)
+lancer_heure.pack()
+
+frame_input_alarm= Frame(Frame_horloge,bg='grey',width=400)
+set_H_alarm=Entry(frame_input_alarm, text="",width=4)
+set_H_alarm.pack(side=LEFT,padx=5)
+set_heure_label_alarm=Label(frame_input_alarm, text="H")
+set_heure_label_alarm.pack(side=LEFT,padx=5)
+set_M_alarm=Entry(frame_input_alarm, text="",width=4)
+set_M_alarm.pack(side=LEFT,padx=5)
+set_minute_label_alarm=Label(frame_input_alarm, text="M")
+set_minute_label_alarm.pack(side=LEFT,padx=5)
+set_S_alarm=Entry(frame_input_alarm, text="",width=4)
+set_S_alarm.pack(side=LEFT,padx=5)
+set_seconde_label_alarm=Label(frame_input_alarm, text="S")
+set_seconde_label_alarm.pack(side=LEFT,padx=5)
+frame_input_alarm.pack(pady=5)
+
+lancer_alarm=Button(Frame_horloge, text="Regler alarme", command=lambda: my_alarm((int(set_H_alarm.get()),int(set_M_alarm.get()),int(set_S_alarm.get()))))
 lancer_alarm.pack(side = TOP,padx=5, pady=5)
-affiche_alarme=Label(Frame_horloge, text="Alarme activé")
+affiche_alarme=Label(Frame_horloge, text="Alarme activé",bg="red")
 lancer_alarm=Button(Frame_horloge, text="Pause/reprise", command= pause)
 lancer_alarm.pack(side = TOP,padx=5, pady=5)
 Frame_horloge.pack()
